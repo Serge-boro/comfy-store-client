226b4fa9153d93a9e4d28c1cd51f505cfb4c444a
Serge-boro <serhiiborodin83@gmail.com>
2024-04-25 19:50:03 -0400

    changes

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -5,19 +5,19 @@ const BASE_URL =
 
 export default axios.create({
   baseURL: BASE_URL,
-  // withCredentials: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
-    //   // 'Access-Control-Allow-Origin': 'http://localhost:5173/',
-  },
+  withCredentials: true,
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Credentials': 'true',
+  //   //   // 'Access-Control-Allow-Origin': 'http://localhost:5173/',
+  // },
 })
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
-  },
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Credentials': 'true',
+  // },
 })
