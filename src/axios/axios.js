79ace2213851f4cab4fe8fda76514ab0ca588054
Serge-boro <serhiiborodin83@gmail.com>
2024-04-24 22:26:31 -0400

    changes

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -5,18 +5,18 @@ const BASE_URL = 'https://comfy-store-back.netlify.app/.netlify/functions/app'
 export default axios.create({
   baseURL: BASE_URL,
   // withCredentials: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
-    //   // 'Access-Control-Allow-Origin': 'http://localhost:5173/',
-  },
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Credentials': 'true',
+  //   //   // 'Access-Control-Allow-Origin': 'http://localhost:5173/',
+  // },
 })
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
-  },
+  // headers: {
+  //   'Access-Control-Allow-Origin': '*',
+  //   'Access-Control-Allow-Credentials': 'true',
+  // },
 })
